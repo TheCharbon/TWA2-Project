aba60967eb6d4cc9e6326c9660ad07a38ee94e62
TheCharbon <Colin.Madore@stu.crc-lennox.qc.ca>
2024-11-21 09:33:40 -0500

    init commit 2

--- server/index.js
+++ server/index.js
@@ -1,33 +1,24 @@
+
 import express from 'express';
-import * as dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import cors from 'cors';
 
-import connectDB from './mongodb/connect.js';
-import postRoutes from './routes/postRoutes.js';
-import dalleRoutes from './routes/dalleRoutes.js';
-
-dotenv.config();
+import postRoutes from './routes/posts.js';
 
 const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
 
-app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/dalle', dalleRoutes);
+app.use('/posts', postRoutes);
 
-app.get('/', async (req, res) => {
-  res.status(200).json({
-    message: 'Hello from DALL.E!',
-  });
-});
+const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const PORT = process.env.PORT|| 5000;
 
-const startServer = async () => {
-  try {
-    connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port 8080'));
-  } catch (error) {
-    console.log(error);
-  }
-};
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
-startServer();
+mongoose.set('useFindAndModify', false);
