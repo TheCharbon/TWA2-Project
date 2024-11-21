9727f23e7e1dd51e0c1bb9204335bdc00774d7d4
TheCharbon <Colin.Madore@stu.crc-lennox.qc.ca>
2024-11-21 09:39:10 -0500

    shell scripts

---
+++ bin/prepareDeploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+cd ../public/
+npm install
+npm run build
+
+rm -rf ../server/react_build || true
+cp -r build/ ../server/react_build
+rm -rf build/
+
+cd ../server
+rm full.zip || true
+7z a -tzip full.zip . -xr!node_modules
+
+echo "preparations completed"
---
+++ bin/publish_app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+./prepareDeploy.sh
+
+aws s3 cp ../server/full.zip s3://snappy-chat-zip/full.zip
+
+VERSION_LABEL="v$(date +%Y%m%d%H%M%S)"
+echo "new version label $VERSION_LABEL"
+
+aws elasticbeanstalk create-application-version --application-name chat-application --version-label $VERSION_LABEL --source-bundle S3Bucket=snappy-chat-zip,S3Key=full.zip --no-cli-pager
+
+aws elasticbeanstalk update-environment --environment-name Chat-application-env --version-label $VERSION_LABEL --no-cli-pager
+
+
+echo "Deploy completed"
